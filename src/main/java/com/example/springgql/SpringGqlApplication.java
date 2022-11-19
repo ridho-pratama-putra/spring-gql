47cab6887493631e7733772689b1a7a1dc0c4a87
ridho <ridho.putra@btpn.com>
2022-11-19 11:53:16 +0700

    logging gql request to console

--- src/main/java/com/example/springgql/SpringGqlApplication.java
+++ src/main/java/com/example/springgql/SpringGqlApplication.java
@@ -2,10 +2,19 @@ package com.example.springgql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.Clock;
+import java.time.ZoneId;
 
 @SpringBootApplication
 public class SpringGqlApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringGqlApplication.class, args);
+	}
+
+	@Bean
+	public Clock systemUtcClock() { // <--Note the method name will change the bean ID
+		return Clock.system(ZoneId.of("Asia/Jakarta"));
 	}
 }
