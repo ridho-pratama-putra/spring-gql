11b92d6b1330091f70d5e81e8622c6820b0341b9
ridho <ridho.putra@btpn.com>
2022-12-04 20:09:38 +0700

    add MDC (Mapped Diagnostic Context) filter as first layer to give additional X-Request-ID in header and later used in service-to-service communication

---
+++ src/main/java/com/example/springgql/config/MDCFilter.java
@@ -0,0 +1,49 @@
+package com.example.springgql.config;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Order(1)
+@Component
+public class MDCFilter implements Filter {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(MDCFilter.class);
+    private final String X_REQUEST_ID = "X-Request-ID";
+
+    @Override
+    public void doFilter(ServletRequest request,
+                         ServletResponse response,
+                         FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        try {
+            addXRequestId(req);
+            LOGGER.info("path: {}, method: {}, query {}",
+                    req.getRequestURI(), req.getMethod(), req.getQueryString());
+            res.setHeader(X_REQUEST_ID, MDC.get(X_REQUEST_ID));
+            chain.doFilter(request, response);
+        } finally {
+            LOGGER.info("statusCode {}, path: {}, method: {}, query {}",
+                    res.getStatus(), req.getRequestURI(), req.getMethod(), req.getQueryString());
+            MDC.clear();
+        }
+    }
+
+    private void addXRequestId(HttpServletRequest request) {
+        String xRequestId = request.getHeader(X_REQUEST_ID);
+        if (xRequestId == null) {
+            MDC.put(X_REQUEST_ID, UUID.randomUUID().toString());
+        } else {
+            MDC.put(X_REQUEST_ID, xRequestId);
+        }
+    }
+}
--- src/main/java/com/example/springgql/services/ArtistLibraryService.java
+++ src/main/java/com/example/springgql/services/ArtistLibraryService.java
@@ -1,15 +1,23 @@
 package com.example.springgql.services;
 
+import com.example.springgql.enums.CategoryEnum;
+import com.example.springgql.models.Album;
 import com.example.springgql.models.Artist;
 import com.example.springgql.models.graphqlInput.ArtistInput;
 import com.example.springgql.repositories.ArtistRepository;
+import org.slf4j.MDC;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 @Service
 public class ArtistLibraryService {
-
     ArtistRepository repository;
 
     ArtistLibraryService(ArtistRepository repository) {
@@ -17,6 +25,17 @@ public class ArtistLibraryService {
     }
 
     public Artist saveArtist(ArtistInput artistInput) {
+        RestTemplate restTemplate = new RestTemplate();
+        SimpleDateFormat date = new SimpleDateFormat("dd/MM/yyyy");
+        HttpEntity<Album> request = null;
+        try {
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.add("X-Request-ID", MDC.get("X-Request-ID"));
+            request = new HttpEntity<>(new Album("krupuk", "rambak", CategoryEnum.ROCK, date.parse("19/08/1945")), httpHeaders);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+        ResponseEntity<Album> albumResponseEntity = restTemplate.postForEntity("http://localhost:8082/album", request, Album.class);
         Artist entity = Artist.builder()
                 .name(artistInput.getName())
                 .albums(null)
