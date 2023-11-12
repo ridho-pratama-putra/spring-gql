37332067254a6153599526eef4d6c8da76060a0d
ridho <ridhopratamavd@gmail.com>
2023-11-12 23:27:23 +0700

    remove test

--- src/test/java/com/example/springgql/services/ArtistServiceTest.java
+++ src/test/java/com/example/springgql/services/ArtistServiceTest.java
@@ -1,12 +1,9 @@
 package com.example.springgql.services;
 
-import com.example.springgql.exception.DataNotDeletedException;
 import com.example.springgql.models.Artist;
-import com.example.springgql.models.Release;
 import com.example.springgql.models.graphqlInput.ArtistInput;
 import com.example.springgql.repositories.ArtistRepository;
 
-import org.junit.Ignore;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -14,18 +11,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 @SpringBootTest
 class ArtistServiceTest {
 
     @Autowired
     ArtistService service;
-    @MockBean
-    ReleaseService releaseService;
 
     @MockBean
     ArtistRepository repository;
@@ -53,20 +45,5 @@ class ArtistServiceTest {
 
         Assertions.assertEquals(1, actualResult.size());
         Assertions.assertEquals(expectedName, actualResult.get(0).getName());
-    }
-
-    @Test
-    @Ignore
-    void deleteById_shouldThrowFailedToDelete_whenArtistHaveSomeReleases() {
-        Assertions.assertThrows(DataNotDeletedException.class, () -> {
-            Artist artist = Artist.builder().name("endank").build();
-            Mockito.when(repository.findById(Mockito.anyString())).thenReturn(Optional.of(artist));
-            Release release = Release.builder().artist(artist).title("happy birthday!").build();
-            Map<Artist, List<Release>> unDeletedRelease = new HashMap<>();
-            unDeletedRelease.put(artist, Arrays.asList(release));
-            Mockito.when(releaseService.getReleasesByArtistIds(Mockito.anyList())).thenReturn(unDeletedRelease);
-
-            service.deleteById("some id matched with deleted record");
-        });
     }
 }
